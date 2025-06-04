b93596dc52f98e44d9c71408098ee4e4ed217189
Ayoyinka2456 <108306542+Ayoyinka2456@users.noreply.github.com>
2025-06-04 02:35:24 -0400

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -9,9 +9,9 @@ echo "Updating kubeconfig for springboot-cluster..."
 aws eks --region us-east-2 update-kubeconfig --name springboot-cluster
 
 echo "Deploying Spring Boot app..."
-kubectl apply -f ../deployment-ingress.yml
+kubectl apply -f deployment-ingress.yml
 
 echo "Deploying Prometheus and Grafana..."
-kubectl apply -f ../prom-graphana.yml
+kubectl apply -f prom-graphana.yml
 
 echo "Deployment complete!"
